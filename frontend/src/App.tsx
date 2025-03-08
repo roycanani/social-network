985150d65296cfbf9e17419f971e784734953123
Roy Canani <roy33441c@gmail.com>
2025-03-08 13:28:44 +0200

    fix unused import

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,3 @@
-import Login from "./pages/Login";
 import SignIn from "./pages/SignIn";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
